6f4271cb1bf353044e92b6d992df79ec073f268e
el ouadi abdelati <abou@ABOUs-MacBook-Pro.local>
2023-09-02 21:35:33 +0100

    init

---
+++ src/main/java/com/example/demo/DemoWhatTheDiffApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWhatTheDiffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWhatTheDiffApplication.class, args);
+	}
+
+}
