dc19e7d31ccba6c5921631c640d8bd7c55da8726
el ouadi abdelati <abou@ABOUs-MacBook-Pro.local>
2023-09-02 21:44:59 +0100

    add user feature

---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,17 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id ;
+
+    @Column
+    private String name;
+}
